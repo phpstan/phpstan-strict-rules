a58717a118523f311627184be6ef4a76d0f28f38
CZechBoY <thejabko@gmail.com>
2018-05-05 18:36:29 +0200

    Check whether method has return typehint

---
+++ src/Rules/Methods/MissingMethodReturnTypehintRule.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Methods;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\MixedType;
+
+final class MissingMethodReturnTypehintRule implements \PHPStan\Rules\Rule
+{
+
+	/**
+	 * @return string Class implementing \PhpParser\Node
+	 */
+	public function getNodeType(): string
+	{
+		return \PhpParser\Node\Stmt\ClassMethod::class;
+	}
+
+	/**
+	 * @param \PhpParser\Node\Stmt\ClassMethod $node
+	 * @param \PHPStan\Analyser\Scope $scope
+	 *
+	 * @return string[] errors
+	 */
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!$scope->isInClass()) {
+			throw new \PHPStan\ShouldNotHappenException();
+		}
+
+		$methodReflection = $scope->getClassReflection()->getNativeMethod($node->name);
+
+		$messages = [];
+
+		$message = $this->checkMethodReturnType($methodReflection);
+		if ($message !== null) {
+			$messages[] = $message;
+		}
+
+		return $messages;
+	}
+
+	private function checkMethodReturnType(MethodReflection $methodReflection): ?string
+	{
+		$returnType = $methodReflection->getReturnType();
+
+		if ($returnType instanceof MixedType && !$returnType->isExplicitMixed()) {
+			return sprintf(
+				'Method %s::%s() has no return typehint specified',
+				$methodReflection->getDeclaringClass()->getName(),
+				$methodReflection->getName()
+			);
+		}
+
+		return null;
+	}
+
+}
