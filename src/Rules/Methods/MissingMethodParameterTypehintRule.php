eecbcc86f6cc7f43fdcd2267e5bc177aacb74e56
CZechBoY <thejabko@gmail.com>
2018-05-05 18:30:58 +0200

    Check whether method parameter has typehint

---
+++ src/Rules/Methods/MissingMethodParameterTypehintRule.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Methods;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParameterReflection;
+use PHPStan\Type\MixedType;
+
+final class MissingMethodParameterTypehintRule implements \PHPStan\Rules\Rule
+{
+
+	/**
+	 * @return string Class implementing \PhpParser\Node
+	 */
+	public function getNodeType(): string
+	{
+		return \PhpParser\Node\Stmt\ClassMethod::class;
+	}
+
+	/**
+	 * @param \PhpParser\Node\Stmt\ClassMethod $node
+	 * @param \PHPStan\Analyser\Scope $scope
+	 *
+	 * @return string[] errors
+	 */
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!$scope->isInClass()) {
+			throw new \PHPStan\ShouldNotHappenException();
+		}
+
+		$methodReflection = $scope->getClassReflection()->getNativeMethod($node->name);
+
+		$messages = [];
+
+		foreach ($methodReflection->getParameters() as $parameterReflection) {
+			$message = $this->checkMethodParameter($methodReflection, $parameterReflection);
+			if ($message === null) {
+				continue;
+			}
+
+			$messages[] = $message;
+		}
+
+		return $messages;
+	}
+
+	private function checkMethodParameter(MethodReflection $methodReflection, ParameterReflection $parameterReflection): ?string
+	{
+		$parameterType = $parameterReflection->getType();
+
+		if ($parameterType instanceof MixedType && !$parameterType->isExplicitMixed()) {
+			return sprintf(
+				'Method %s::%s() has parameter $%s with no typehint specified',
+				$methodReflection->getDeclaringClass()->getName(),
+				$methodReflection->getName(),
+				$parameterReflection->getName()
+			);
+		}
+
+		return null;
+	}
+
+}
