b94a0b13fb87d8d034c103852656118b32df9155
Mikko Pesari <mpesari@users.noreply.github.com>
2026-02-11 11:03:17 +0200

    Bleeding edge - Add operand types to DisallowedLooseComparisonRule error message

--- tests/Rules/DisallowedConstructs/DisallowedLooseComparisonRuleTest.php
+++ tests/Rules/DisallowedConstructs/DisallowedLooseComparisonRuleTest.php
@@ -11,13 +11,18 @@ use PHPStan\Testing\RuleTestCase;
 class DisallowedLooseComparisonRuleTest extends RuleTestCase
 {
 
+	private bool $includeOperandTypesInErrorMessage;
+
 	protected function getRule(): Rule
 	{
-		return new DisallowedLooseComparisonRule();
+		return new DisallowedLooseComparisonRule(
+			$this->includeOperandTypesInErrorMessage,
+		);
 	}
 
-	public function testRule(): void
+	public function testRuleWithoutOperandTypesInErrorMessage(): void
 	{
+		$this->includeOperandTypesInErrorMessage = false;
 		$this->analyse([__DIR__ . '/data/weak-comparison.php'], [
 			[
 				'Loose comparison via "==" is not allowed.',
@@ -27,6 +32,83 @@ class DisallowedLooseComparisonRuleTest extends RuleTestCase
 			[
 				'Loose comparison via "!=" is not allowed.',
 				5,
+				'Use strict comparison via "!==" instead.',
+			],
+			[
+				'Loose comparison via "==" is not allowed.',
+				8,
+				'Use strict comparison via "===" instead.',
+			],
+			[
+				'Loose comparison via "!=" is not allowed.',
+				10,
+				'Use strict comparison via "!==" instead.',
+			],
+			[
+				'Loose comparison via "==" is not allowed.',
+				13,
+				'Use strict comparison via "===" instead.',
+			],
+			[
+				'Loose comparison via "!=" is not allowed.',
+				15,
+				'Use strict comparison via "!==" instead.',
+			],
+			[
+				'Loose comparison via "==" is not allowed.',
+				18,
+				'Use strict comparison via "===" instead.',
+			],
+			[
+				'Loose comparison via "!=" is not allowed.',
+				20,
+				'Use strict comparison via "!==" instead.',
+			],
+		]);
+	}
+
+	public function testRuleWithOperandTypesInErrorMessage(): void
+	{
+		$this->includeOperandTypesInErrorMessage = true;
+		$this->analyse([__DIR__ . '/data/weak-comparison.php'], [
+			[
+				'Loose comparison via "==" between int and int is not allowed.',
+				3,
+				'Use strict comparison via "===" instead.',
+			],
+			[
+				'Loose comparison via "!=" between int and int is not allowed.',
+				5,
+				'Use strict comparison via "!==" instead.',
+			],
+			[
+				'Loose comparison via "==" between DateTime and float is not allowed.',
+				8,
+				'Use strict comparison via "===" instead.',
+			],
+			[
+				'Loose comparison via "!=" between float and DateTime is not allowed.',
+				10,
+				'Use strict comparison via "!==" instead.',
+			],
+			[
+				'Loose comparison via "==" between DateTime and null is not allowed.',
+				13,
+				'Use strict comparison via "===" instead.',
+			],
+			[
+				'Loose comparison via "!=" between null and DateTime is not allowed.',
+				15,
+				'Use strict comparison via "!==" instead.',
+			],
+			[
+				'Loose comparison via "==" between DateTime and DateTime is not allowed.',
+				18,
+				'Use strict comparison via "===" instead.',
+			],
+			[
+				'Loose comparison via "!=" between DateTime and DateTime is not allowed.',
+				20,
 				'Use strict comparison via "!==" instead.',
 			],
 		]);
